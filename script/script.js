60a2bc44655f4d842692503d96f2e98a9e97e6ba
Gustavo Murilo <57266489+GustavoMSB@users.noreply.github.com>
2021-08-26 19:19:34 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,6 @@
 const button = document.getElementById('btnativar');
 const navbar = document.getElementById('navbar');
 
-/* navbar.style.display = "none"; */
-/* const navBar = */ 
-
 button.addEventListener('click', () => {
     if(navbar.style.display == "none"){
         navbar.style.display = "block";
@@ -11,13 +8,4 @@ button.addEventListener('click', () => {
         navbar.style.display = "none";
     }
     
-})
-
-navbar.addEventListener('click', () => {
-    console.log('Funfou');
-    
-       
-     /*   navBar.style.display = 'block';  */
-     console.log('Funfou2');
-    
-})
+});
